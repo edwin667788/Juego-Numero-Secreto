9a84d0ea9523cda37bea6b3ab24c9d1030dfd1b7
Edwin Pabon <edwin.pab2@gmail.com>
2023-08-31 18:37:57 -0500

    se incorporan los intentos ateriores

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+const numeroMaximoIntentos = 5; // Número máximo de intentos permitidos
+let numeroAleatorio = generarNumeroAleatorio(); // Generar un nuevo número aleatorio
 let intentos = 0;
 
 const numeroInput = document.getElementById("numeroInput");
 const adivinarBtn = document.getElementById("adivinarBtn");
 const mensaje = document.getElementById("mensaje");
+const reiniciarBtn = document.getElementById("reiniciarBtn");
 
+// Función para generar un nuevo número aleatorio
+function generarNumeroAleatorio() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Función para reiniciar el juego
+function reiniciarJuego() {
+  intentos = 0;
+  numeroAleatorio = generarNumeroAleatorio();
+  mensaje.textContent = "";
+  numeroInput.value = "";
+  adivinarBtn.disabled = false;
+  reiniciarBtn.disabled = true;
+}
+
+// Evento de clic en el botón "Adivinar"
 adivinarBtn.addEventListener("click", function () {
   const numeroJugador = parseInt(numeroInput.value);
   intentos++;
@@ -16,5 +34,15 @@ adivinarBtn.addEventListener("click", function () {
   } else {
     mensaje.textContent = `¡Felicidades! Adivinaste el número en ${intentos} intentos.`;
     adivinarBtn.disabled = true;
+    reiniciarBtn.disabled = false;
+  }
+
+  if (intentos >= numeroMaximoIntentos) {
+    mensaje.textContent = `Agotaste tus intentos. El número era ${numeroAleatorio}.`;
+    adivinarBtn.disabled = true;
+    reiniciarBtn.disabled = false;
   }
 });
+
+// Evento de clic en el botón "Reiniciar"
+reiniciarBtn.addEventListener("click", reiniciarJuego);
